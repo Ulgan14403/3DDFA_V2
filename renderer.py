f72af0958a621bffcf064ae3c588e36f2ab1fd40
ulgan14403 <antdes@hotmail.fr>
2025-02-26 15:55:30 -0500

    Image de fond ajoutee

--- renderer.py
+++ renderer.py
@@ -1,16 +1,35 @@
+import trimesh.visual
 import pyrender
 import trimesh 
 import numpy as np
 
+def param_intrinsics():
+    '''
+    Donne les paramètres intrinsèques de la caméra (RealSense D415) 
+    
+    '''
+    pipeline = rs.pipeline()
+    config = rs.config()
+    cfg = pipeline.start(config)
+    profile=(cfg.get_stream(rs.stream.color,0))
+    intr = profile.as_video_stream_profile().get_intrinsics()
+    return(intr)
 
-'''
+def Mat_intr(intr):
+    '''
+    Crée la matrice intrinsèque d'une caméra à partir de ses caractéristiques intrinsèques
+    '''
+    camMat = np.eye(3)
+    camMat[0,0] = intr.fx
+    camMat[1,1] = intr.fy
+    camMat[0,2] = intr.ppx
+    camMat[1,2] = intr.ppy
+    return(camMat)
+#Render avec pyrender
+''''
 def render(tm):
     m = pyrender.Mesh.from_trimesh(tm)
     
-    
-    #Per Face / Per vertex coloration #TODO ajuster avec les indices adaptés
-    #tm.visual.vertex_colors = np.random.uniform(size=tm.vertices.shape)
-    #tm.visual.face_colors = np.random.uniform(size=tm.faces.shape)
     mesh = pyrender.Mesh.from_trimesh(tm)
     print(mesh.primitives)
     
@@ -45,8 +64,8 @@ def render(tm):
     
     pyrender.Viewer(scene,face_normals=True)
     return(0)
+'''
     
-    '''
 
 import numpy as np
 import open3d as o3d
@@ -54,27 +73,9 @@ import open3d.visualization.rendering as rendering
 import cv2
 import imageio
 import pyrealsense2 as rs
-
-def param_intrinsics():
-    '''
-    Donne les paramètres intrinsèques de la caméra (RealSense D415) 
-    
-    '''
-    pipeline = rs.pipeline()
-    config = rs.config()
-    cfg = pipeline.start(config)
-    profile=(cfg.get_stream(rs.stream.color,0))
-    intr = profile.as_video_stream_profile().get_intrinsics()
-    return(intr)
-
-def Mat_intr(intr):
-    camMat = np.eye(3)
-    camMat[0,0] = intr.fx
-    camMat[1,1] = intr.fy
-    camMat[0,2] = intr.ppx
-    camMat[1,2] = intr.ppy
-    return(camMat)
-    
+from PIL import Image
+#Renderer avec open3d
+''' 
 def render(img):
     
     
@@ -91,8 +92,8 @@ def render(img):
     if bg_image is None:
         print("Erreur : Impossible de lire l'image 'background.jpg'. Assure-toi que le fichier existe et est au bon format.")
     else:
-
-        bg_color = np.array([0.0, 1.0, 1.0, 0.5], dtype=np.float32)  # Format correct (RGBA)
+        print(vis)
+        bg_color = np.array([255.0, 255.0, 255.0, 0.5], dtype=np.float32)  # Format correct (RGBA)
         
         vis.set_background(bg_color, None)
         
@@ -113,7 +114,7 @@ def render(img):
     finally:
         app.quit()  # Assure la fermeture correcte pour éviter le freeze
 
-    pass
+    return(0)
     
     intr = param_intrinsics()
     camMat = Mat_intr(intr)
@@ -159,14 +160,101 @@ def render(img):
     img_cv2 = cv2.cvtColor(np.array(img_o3d), cv2.COLOR_RGBA2BGR)
     cv2.imshow("Preview window", img_cv2)
     cv2.waitKey()
+    '''
+    
+import copy
+    
+def render(tm,img):
+    
+    #Creation du mesh pour pyrender
+    mesh = pyrender.Mesh.from_trimesh(tm)
+    
+    #Creating lights
+    pl = pyrender.PointLight(color=[1.0, 1.0, 1.0], intensity=2.0)
+    dl = pyrender.DirectionalLight(color=[1.0, 1.0, 1.0], intensity=2.0)
+    
+    #Creating camera
+    intr = param_intrinsics()
+    intr.height = img.height #optimize paramètres pour l'image
+    intr.width  = img.width 
+    oc = pyrender.camera.IntrinsicsCamera(fx=intr.fx,fy=intr.fy,cx=960,cy=510,zfar=10000,name='main_camera')
+    
+    #Creation de la scene
+    scene = pyrender.Scene(ambient_light=[0.3, 0.3, 0.3],bg_color=[1.0, 0.0, 0.0])
     
     
+    #Creation de «l'écran» 
+    edge_lengths = np.array([np.shape(img)[1],np.shape(img)[0],1.0],np.float64)
+    print(edge_lengths)
+    scr_transform = np.eye(4)
+    screen = trimesh.creation.box(extents=edge_lengths,transform=scr_transform,bounds=None)
     
+    
+    #Creation de la texture du screen
+    uv= [
+         [0.0, 0.0],
+         [0.0, 0.0],
+         [0.0, 0.0],
+         [0.0, 1.0],
+         [0.0, 0.0],
+         [1.0, 0.0],
+         [0.0, 0.0],  
+         [1.0, 1.0],
+    ]
+    
+    material = trimesh.visual.texture.SimpleMaterial(image=img)
+    color_visuals = trimesh.visual.TextureVisuals(uv=uv, image=img, material=material)
+    screen=trimesh.Trimesh(vertices=screen.vertices, faces=screen.faces, visual=color_visuals, validate=True, process=False)
+    screen = pyrender.Mesh.from_trimesh(screen)
+    
+    
+    #Creation des nodes
+    pose_mesh = np.eye(4)
+    pose_camera = np.eye(4)
+    print(img.width)
+    z = max((616/1920)*img.width,(616/1080)*img.height) #ajuste la caméra à la taille de l'image
+    pose_camera[:3,3]=[0,0,z] 
+    pose_pl = copy.deepcopy(pose_camera)
+    pose_screen = np.eye(4)
+    
+    
+    npl = pyrender.Node(light=pl,matrix = pose_pl)
+    nm = pyrender.Node(mesh=mesh, matrix=pose_mesh)
+    nl = pyrender.Node(light=dl, matrix=pose_camera )
+    nc = pyrender.Node(camera=oc, matrix=pose_camera)
+    n_scr = pyrender.Node(mesh=screen,matrix = pose_screen)
+    
+    #Ajout des nodes
+    scene.add_node(npl)
+    scene.add_node(nm)
+    scene.add_node(nl)
+    scene.add_node(nc)
+    scene.add_node(n_scr)
+    
+    #Creer offscreenRenderer
+    r=pyrender.OffscreenRenderer(viewport_width=intr.width,
+                               viewport_height=intr.height,
+                               point_size=1.0)
+    
+    
+    color, depth = r.render(scene)
+    
+    r.delete()
+    
+    #Afficher l'image
+    color = cv2.cvtColor(color,cv2.COLOR_RGB2BGR)
+    cv2.imshow('fenetre',color)
+    cv2.waitKey()
+    return(0)
+
+
+
 if __name__ == '__main__':
     
     #tm= trimesh.load(r"E:\Antoine\OneDrive - ETS\Program_Files\GitHubs\3DDFA_V2\masque.stl")
-    #tm = trimesh.creation.cylinder(5,10)
+    tm = trimesh.creation.cylinder(5,10)
     #print(tm)
-    img = imageio.imread(r"E:\Antoine\OneDrive - ETS\Program_Files\GitHubs\3DDFA_V2\image_fond.PNG")
-    img = np.asarray(img)
-    render(img)
+    img = Image.open(r"E:\Antoine\OneDrive - ETS\Program_Files\GitHubs\3DDFA_V2\image_fond.PNG")
+    
+    #print(img.type)
+    render(tm,img)
